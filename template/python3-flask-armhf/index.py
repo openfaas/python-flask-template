60b54f86792fbd6d2393f88e5e7f192b619b09e5
sjsullivan7 <sjsullivan7@gmail.com>
2018-10-17 22:38:55 -0400

    Resolves ISSUE #5
    Added new method to index.py to fix an issue with chunked
    encoding and missing request bodies
    
    Signed-off-by: Stephen Sullivan <sjsullivan7@gmail.com>

--- template/python3-flask-armhf/index.py
+++ template/python3-flask-armhf/index.py
@@ -8,6 +8,18 @@ from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
+@app.before_request
+def fix_transfer_encoding():
+    """
+    Sets the "wsgi.input_terminated" environment flag, thus enabling
+    Werkzeug to pass chunked requests as streams.  The gunicorn server
+    should set this, but it's not yet been implemented.
+    """
+
+    transfer_encoding = request.headers.get("Transfer-Encoding", None)
+    if transfer_encoding == u"chunked":
+        request.environ["wsgi.input_terminated"] = True
+
 @app.route("/", defaults={"path": ""}, methods=["POST", "GET"])
 @app.route("/<path:path>", methods=["POST", "GET"])
 def main_route(path):
