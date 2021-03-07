fef5672b8c43154f984ca8f11da7e02bfea87b62
Lucas Roesler <roesler.lucas@gmail.com>
2021-03-07 19:14:20 +0100

    Use tox to test the python3-flask template
    **What**
    - Add a sample tox.ini file to the python3-flask. This configuration
      will run pytest and flake8 for the function handler. The default
      flake8 is very resticted, only erroring for undefined names and
      syntax errors. This allows us to lint and test the function during
      `faas-cli build`, which should simplify CI flows for users. Using
      `tox` allows th euser to completely customize the test flow without
      requiring any changes to the template.
    - Add same test for the echo handler
    - Include documentationthat describes how to disable
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

---
+++ template/python3-flask/function/handler_test.py
@@ -0,0 +1,8 @@
+from .handler import handle
+
+# Test your handler here
+# To disable testing, you need to update the
+# tox.ini file, in addition to deleting this file.
+
+def test_handle():
+    assert handle("input") == "input"
